f8d4bf2daa31ccc50b7c6609dcbda69ec32ecd82
shaojung <shaojung@gmail.com>
2016-10-25 18:51:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvn102501/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.yvtc.yvn102501;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        Log.d("MYLOG", "This is click1");
+    }
+}
