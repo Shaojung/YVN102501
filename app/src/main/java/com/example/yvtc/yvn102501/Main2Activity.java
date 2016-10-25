7f703564e7b0e56b53b3c863d4e93b446cd9a3a9
shaojung <shaojung@gmail.com>
2016-10-25 19:52:10 +0800

    判斷 View

--- app/src/main/java/com/example/yvtc/yvn102501/Main2Activity.java
+++ app/src/main/java/com/example/yvtc/yvn102501/Main2Activity.java
@@ -8,18 +8,28 @@ import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity
     implements View.OnClickListener {
-    Button btn3;
+    Button btn3, btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         btn3 = (Button) findViewById(R.id.button3);
+        btn4 = (Button) findViewById(R.id.button4);
         btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Log.d("MYLOG", "這是按鈕三");
+        if (v.getId() == R.id.button3)
+        {
+            Log.d("MYLOG", "這是按鈕三");
+        }
+        else if (v.getId() == R.id.button4)
+        {
+            Log.d("MYLOG", "這是按鈕四");
+        }
+
     }
 }
